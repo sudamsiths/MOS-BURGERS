fd6f341df88dbfbb5a2cdff2dc5fda6968e1bb30
sudamsiths <sudamsiths.com>
2025-05-20 20:50:53 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.querySelector('.md\\:hidden.p-2');
-    const mobileMenu = document.querySelector('.md\\:hidden.hidden.pt-4');
-    
-    menuButton.addEventListener('click', function() {
-      mobileMenu.classList.toggle('hidden');
-      mobileMenu.classList.toggle('active');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.querySelector('.md\\:hidden.p-2');
+  const mobileMenu = document.querySelector('.md\\:hidden.hidden.pt-4');
+
+  menuButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('active');
   });
-  var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+});
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-// Add this script to your HTML file or in a separate JS file
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const darkModeToggle = document.querySelector('.dark-mode-toggle');
-  
-  // Check if there's a saved preference in localStorage
+
   if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
   }
-  
-  // Add click event listener to the toggle button
-  darkModeToggle.addEventListener('click', function() {
-    // Toggle dark-mode class on the body
+
+  darkModeToggle.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-    
-    // Save preference to localStorage
+
     if (document.body.classList.contains('dark-mode')) {
       localStorage.setItem('darkMode', 'enabled');
     } else {
@@ -35,3 +30,23 @@ function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
 }
 lightGallery(document.getElementById('lg-srcset-demo'));
+    document.getElementById('newsletter-form').addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById('email-address').value;
+      const successMessage = document.getElementById('success-message');
+
+      if (email) {
+        successMessage.style.display = 'flex';
+
+        document.getElementById('email-address').value = '';
+
+        setTimeout(() => {
+          successMessage.style.display = 'none';
+        }, 5000);
+      }
+    });
+
+    document.getElementById('close-btn').addEventListener('click', function () {
+      document.getElementById('success-message').style.display = 'none';
+    });
